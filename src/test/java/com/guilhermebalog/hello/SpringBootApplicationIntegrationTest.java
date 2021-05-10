b77e0d788ea2945f8432a120af9d3f69395fbcd3
Guilherme Balog Gardino <ggardino@uolinc.com>
2021-05-10 16:49:48 -0300

    test: integration test using mock mvc

---
+++ src/test/java/com/guilhermebalog/hello/SpringBootApplicationIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.guilhermebalog.hello;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = HelloApplication.class)
+@WebAppConfiguration
+public class SpringBootApplicationIntegrationTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setupMockMvc(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenRequestHasBeenMade_whenMeetsAllOfGivenConditions_thenCorrect() throws Exception {
+        MediaType contentType = new MediaType(
+                MediaType.APPLICATION_JSON.getType(),
+                MediaType.APPLICATION_JSON.getSubtype()
+        );
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/books"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(contentType))
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray());
+    }
+}
